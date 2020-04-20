a36e41934bb83813558cc57d234ea6e07cfbbfb9
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-04-20 17:52:04 +0500

    realizuem static methods s pomoshi delegate

--- Calculator.cs
+++ Calculator.cs
@@ -7,16 +7,19 @@ namespace MyprojecsApp
         public DelCalculator OpMinus = Minus;
         public DelCalculator OpMultiply = Multiply;
         public DelCalculator OpDivide = Divide;
-        private static T Plus(T arg_1, T arg_2)
+        private static T Plus(T arg_1, T arg_2)         //Вызов методов происходит только по средством делегата. по этому private
         {
+            System.Console.Write($"{arg_1} + {arg_2} = ");
             return (dynamic)arg_1 + (dynamic)arg_2;
         }
         private static T Minus(T arg_1, T arg_2)
         {
+            System.Console.Write($"{arg_1} - {arg_2} = ");
             return (dynamic)arg_1 - (dynamic)arg_2;
         }
         private static T Multiply(T arg_1, T arg_2)
         {
+            System.Console.Write($"{arg_1} * {arg_2} = ");
             return (dynamic)arg_1 * (dynamic)arg_2;
         }
         private static T Divide(T arg_1, T arg_2)
@@ -26,7 +29,11 @@ namespace MyprojecsApp
                 System.Console.Write($"Ошибка: Деление на нол!!!\t{arg_1} / ");
                 return (dynamic)arg_2;
             }
-            else return (dynamic)arg_1 / (dynamic)arg_2;
+            else
+            {
+                System.Console.Write($"{arg_1} / {arg_2} = ");
+                return (dynamic)arg_1 / (dynamic)arg_2;
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,8 +6,40 @@ namespace MyprojecsApp
     {
         static void Main(string[] args)
         {
-            Calculator<double> A=new Calculator<double>();
-            Console.ReadKey();
+            Calculator<double> Operation = new Calculator<double>();
+            while (true)
+            {
+                Console.Write("Аргумент_1: "); double arg_1= double.Parse(Console.ReadLine());
+                Console.Write("Аргумент_2: "); double arg_2 = double.Parse(Console.ReadLine());
+                System.Console.WriteLine("Выберите действия(+ , - , * , / , e); e-выход");
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        {
+                            Console.WriteLine(Operation.OpPlus(arg_1, arg_2));
+                        }
+                        break;
+                    case "-":
+                        {
+                            Console.WriteLine(Operation.OpMinus(arg_1, arg_2));
+                        }
+                        break;
+                    case "*":
+                        {
+                            Console.WriteLine(Operation.OpMultiply(arg_1, arg_2));
+                        }
+                        break;
+                    case "/":
+                        {
+                            Console.WriteLine(Operation.OpDivide(arg_1, arg_2));
+                        }
+                        break;
+                    case "e":
+                        {
+                            return;
+                        }
+                }
+            }
         }
     }
 }
